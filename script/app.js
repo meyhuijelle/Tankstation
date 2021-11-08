66d666ce2612df2045c41da0573dc619fe7876d6
MeyhuiJelle <jelle.meyhui@student.howest.be>
2021-11-08 09:46:06 +0100

    contactSection

--- script/app.js
+++ script/app.js
@@ -1,11 +1,17 @@
 const listenToSideNav = function() {
     let toggleTrigger = document.querySelectorAll(".js-toggle-nav");
+    let contactNav = document.querySelector(".js-contact");
                 for (let i = 0; i < toggleTrigger.length; i++) {
                     toggleTrigger[i].addEventListener("click", function() {
                         console.log("Er is op het hamburgermenu gedrukt!");
                         document.querySelector("body").classList.toggle("has-mobile-nav");
                     })
                 }
+    contactNav.addEventListener('click', function(){
+        console.log("er is op me gedrukt!");
+        contactNav.classList.toggle("c-contactSection");
+    })
+
 }
 
 document.addEventListener('DOMContentLoaded', function() {
