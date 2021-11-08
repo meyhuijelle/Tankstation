d44fa3529b9831c33834301f549f26a645138914
MeyhuiJelle <jelle.meyhui@student.howest.be>
2021-11-08 19:36:37 +0100

    buttons

--- script/app.js
+++ script/app.js
@@ -1,6 +1,10 @@
 const listenToSideNav = function() {
     let toggleTrigger = document.querySelectorAll(".js-toggle-nav");
     let contactNav = document.querySelector(".js-contact");
+    let euro95btn = document.querySelector(".js-euro95");
+    let euro98btn = document.querySelector(".js-euro98");
+    let dieselbtn = document.querySelector(".js-diesel");
+
                 for (let i = 0; i < toggleTrigger.length; i++) {
                     toggleTrigger[i].addEventListener("click", function() {
                         console.log("Er is op het hamburgermenu gedrukt!");
@@ -11,6 +15,26 @@ const listenToSideNav = function() {
         console.log("er is op me gedrukt!");
         contactNav.classList.toggle("c-contactSection");
     })
+    euro95btn.addEventListener('click', function(){
+        console.log("er is gedrukkkkktttt!!");
+        euro95btn.classList.toggle('u-euro95');
+        euro95btn.classList.toggle('is-pressed');
+    })
+
+    euro98btn.addEventListener('click', function(){
+        console.log("er is gedrukkkkktttt!!");
+        euro98btn.classList.toggle('u-euro98');
+        euro98btn.classList.toggle('is-pressed');
+    })
+
+    dieselbtn.addEventListener('click', function(){
+        console.log("er is gedrukkkkktttt!!");
+        dieselbtn.classList.toggle('u-diesel');
+        dieselbtn.classList.toggle('is-pressed');
+    })
+
+
+
 
 }
 
